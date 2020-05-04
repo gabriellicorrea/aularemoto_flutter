eb7ed673bb7bdcd5c5d57107f02d5c17d4f2d1fb
Gabrielli Correa <gabriellicorrea7@gmail.com>
2020-05-04 20:39:18 -0300

    aula 1

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,37 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Frase do Dia!!!"),
+        centerTitle: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Text(
+            "Frase do Dia",
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.blue, fontSize: 30.0),
+          ),
+          RaisedButton(
+              color: Colors.blue,
+              child: Text("Nova frase!",
+                  style: TextStyle(color: Colors.yellow, fontSize: 30.0)),
+              onPressed: () {})
+        ],
+      ),
+    );
   }
 }
